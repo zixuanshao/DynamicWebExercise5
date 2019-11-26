f4fae438b304bfd5a9afdccdcf977d7688e84fc7
Claudia Shao <zxshao99@gmail.com>
2019-11-26 14:53:33 -0500

    netlify

--- exercise-five/src/App.js
+++ exercise-five/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+
+// import Login from './pages/Login/index'
+// import UserProfile from './pages/UserProfile/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Router>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/user" component={UserProfile} />
+      </Router> */}
     </div>
   );
 }
