9dccf03e8d7eb18fb7a1b834de2710c82797c58f
Claudia Shao <zxshao99@gmail.com>
2019-11-26 16:01:28 -0500

    moved exercise 5 out

---
+++ build/precache-manifest.29691e9262c4c00fa82d127f2e34862b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29def9e3d0dad3fbd23a131a23cf6d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d111439ba5ac2c068a48",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0eb1a93a7f9ce72bc3a4",
+    "url": "/static/js/2.bb14decd.chunk.js"
+  },
+  {
+    "revision": "d111439ba5ac2c068a48",
+    "url": "/static/js/main.ccb129c1.chunk.js"
+  },
+  {
+    "revision": "51617a8d465025ea34b0",
+    "url": "/static/js/runtime-main.38ec70fb.js"
+  }
+]);
